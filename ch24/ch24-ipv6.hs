c808dbf362fee98f3fe1ddd9ba43197325a9bfed
Justin Unger <justin_unger@apple.com>
2017-03-29 13:54:02 -0700

    don't rely on Integer for dealing with 128bit IPv6 addresses

--- ch24/ch24-ipv6.hs
+++ ch24/ch24-ipv6.hs
@@ -5,20 +5,20 @@ import Data.Char
 import Control.Applicative
 import Data.List
 
+-- i guess Word64 is as big as we have?
 data IPAddress6 = IPAddress6 Word64 Word64
     deriving (Eq, Ord)
 
-toInteger' (IPAddress6 h l) = (toInteger h) * ((toInteger (maxBound :: Word64))+1) + (toInteger l)
 
-foldNibbles :: [Word8] -> Integer
-foldNibbles xs = foldl (\acc x -> (fromIntegral x) + (shiftL acc 4)) 0 xs
+foldNibbles :: [Word8] -> Word64
+foldNibbles xs
+    | length xs > 16 = error "foldNibbles overflow"
+    | otherwise = foldl (\acc x -> (fromIntegral x) + (shiftL acc 4)) 0 xs 
 
-foldWords :: [Word16] -> Integer
-foldWords = foldl (\acc x -> (fromIntegral x) + (shiftL acc 16)) 0
-
-toWord128 :: Integer -> (Word64,Word64)
-toWord128 x = let (l,h) = divMod x ((fromIntegral (maxBound :: Word64))+1) in
-    (fromIntegral l, fromIntegral h)
+foldWords :: [Word16] -> Word64
+foldWords xs
+    | length xs > 4 = error "foldWords overflow"
+    | otherwise = foldl (\acc x -> (fromIntegral x) + (shiftL acc 16)) 0 xs
 
 parseHexDigit :: Parser Char
 parseHexDigit = oneOf hexChars <|> oneOf (toUpper <$> hexChars)
@@ -54,7 +54,9 @@ parseWords = do
 parseAddr6 :: Parser IPAddress6
 parseAddr6 = do
     ws <- parseWords
-    return $ uncurry IPAddress6 $ toWord128 $ foldWords ws
+    let h = foldWords (take 4 ws)
+        l = foldWords (drop 4 ws)
+    return $ IPAddress6 h l
 
 foo :: [Maybe a] -> [a]
 foo [] = []
@@ -65,34 +67,26 @@ foo (x:xs) = case x of
 hexChars :: [Char]
 hexChars = ['0'..'9'] ++ ['a'..'f']
 
-hexValues :: [(Char,Int)]
+hexValues :: [(Char,Word8)]
 hexValues = zip hexChars [0..]
 
 hexValue :: Char -> Word8
 hexValue c = fromIntegral $ maybe 0 id (lookup c hexValues)
 
---hexChar :: Integer -> Char
+hexChar :: Word8 -> Char
 hexChar x = fst (hexValues !! (fromIntegral x))
 
-words16 :: Integral a => a -> [a]
-words16 = baseDigits (2^16)
-
-showWord16 :: Integer -> String
-showWord16 = padTo '0' 4 . map hexChar . nibbles
+showWord16 :: Word16 -> String
+showWord16 = padTo '0' 4 . map hexChar . nibblesBE
 
 showAddr6 :: IPAddress6 -> String
-showAddr6 = concat . intersperse ":" . map showWord16 . words16 . toInteger'
+showAddr6 = concat . intersperse ":" . map showWord16 . addr6ToWords
 
-showWord16' :: Integral a => a -> String
-showWord16' = padTo '0' 4 . map hexChar . nibbles
+addr6ToWords :: IPAddress6 -> [Word16]
+addr6ToWords (IPAddress6 h l) = padTo 0 4 highWords ++ padTo 0 4 loWords
+    where highWords = word64ToWords16BE h
+          loWords   = word64ToWords16BE l
 
-showAddr6' :: IPAddress6 -> String
-showAddr6' = concat . intersperse ":" . map showWord16' . addr6ToWords
-
-addr6ToWords :: IPAddress6 -> [Word64]
-addr6ToWords (IPAddress6 h l) = padTo 0 4 (words16 h) ++ padTo 0 4 (words16 l)
-
---padTo :: Char -> Int -> String -> String
 padTo :: a -> Int -> [a] -> [a]
 padTo c l xs = go (length xs) xs 
     where go l' xs
@@ -102,15 +96,29 @@ padTo c l xs = go (length xs) xs
 instance Show IPAddress6 where
     show = showAddr6
 
---nibbles :: Integer -> [Integer]
-nibbles :: Integral a => a -> [a]
-nibbles = baseDigits (2^4)
+-- break 16bit into 4 bit words, little end first
+nibblesLE :: Word16 -> [Word8]
+nibblesLE 0 = []
+nibblesLE w16 = r : nibblesLE q
+    where q = w16 `shiftR` 4
+          r = fromIntegral $ w16 .&. 0xf
 
---baseDigits :: Integer -> Integer -> [Integer]
-baseDigits _ 0 = []
-baseDigits b n =
-    let (q,r) = divMod n b in
-        baseDigits b q ++ [r]
+nibblesBE = reverse . nibblesLE
+
+-- break 64bit word into 16 bit words, little end first
+word64ToWords16LE :: Word64 -> [Word16]
+word64ToWords16LE 0 = []
+word64ToWords16LE w64 = r : word64ToWords16LE q
+    where q = w64 `shiftR` 16
+          r = fromIntegral $ w64 .&. 0xffff
+
+-- big end first
+word64ToWords16BE = reverse . word64ToWords16LE
+
+-- use Integer just for test harness
+toInteger' :: IPAddress6 -> Integer
+toInteger' (IPAddress6 h l) =
+    ((fromIntegral h) `shiftL` 64) + fromIntegral l
 
 testV6Parse (s,i) = case parseString (toInteger' <$> parseAddr6) mempty s of
     Failure _ -> False
@@ -127,6 +135,11 @@ v6tests = [
 
 
 runV6Tests = all testV6Parse v6tests
+
+addr6 :: String -> Maybe IPAddress6
+addr6 s = case parseString parseAddr6 mempty s of
+    Failure _ -> Nothing
+    Success r -> Just r
 
 main = do
     putStrLn $ "Passes Parse Tests: " ++ show runV6Tests
